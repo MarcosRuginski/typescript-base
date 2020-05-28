28be7dfd3478c45090f98fc220fc80b8f94e8847
MarcosRuginski <marcosrobertoruginski@gmail.com>
2020-05-28 19:05:55 -0300

    typing

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'marcos',
+    password: '12346',
+    techs: [
+      'Node', 
+      'React', 
+      'Native',
+      { title: 'Javascript', experience: 100 },
+    ],
+  });
+
   return response.json({message: 'Hello World'});
 }
